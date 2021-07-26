221039dbb5d321bfc8381920b16a5a1560888452
ankje <l>
2021-07-26 13:40:09 +0800

    git init

---
+++ src/Laravel5I18nProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ankje\Laravel5I18n;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel5I18nProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        echo ">>>>>>>>>>>>!1111111111111111111";
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        echo ">>>>>>>>>>>>!222222222222222222222";
+    }
+}
