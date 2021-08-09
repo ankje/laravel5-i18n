dfc5de27900f06c49f629fc8812683d64cf402cc
ankje <l>
2021-08-10 01:18:34 +0800

    不是纯模块键且不以模块开头的翻译结果直接输出

--- src/I18nHelper.php
+++ src/I18nHelper.php
@@ -3,6 +3,7 @@
 namespace Jy\Laravel5I18n;
 
 use Illuminate\Support\NamespacedItemResolver;
+use Illuminate\Support\Str;
 
 class I18nHelper
 {
@@ -10,13 +11,23 @@ class I18nHelper
         $defaultModule = 'common';
         $segments = (new NamespacedItemResolver)->parseKey($key);
         $module = $segments[1];
+        $isAmoduleKey = false;
         //如果解析出的模块就使用默认模块
         if(!app('jy.translation.loader')->existsModule($module)){
             $key = $defaultModule.'.'.$key;
         }elseif($key==$module){
+            $isAmoduleKey = true;
             $key = ($segments[0]??'*').'::'.($segments[1]??'*').'.'.$key;
         }
         $ret = app('jy.translator')->getFromJson($key, $replace, $locale);
+
+        if(!$isAmoduleKey){
+            $segments = (new NamespacedItemResolver)->parseKey($key);
+            if(!Str::startsWith($ret,$segments[1])){
+                return $ret;
+            }
+        }        
+
         if($ret){
             $segments = (new NamespacedItemResolver)->parseKey($ret);
         }
